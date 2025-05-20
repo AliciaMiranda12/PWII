141ce3268cd593ed1ec75f6d69594de630d852c0
AliciaMiranda12 <mirandacaly@gmail.com>
2025-05-20 16:21:36 -0300

    navbar

--- PDO_CRUD/public/usuario/read.php
+++ PDO_CRUD/public/usuario/read.php
@@ -4,19 +4,20 @@ include '../../config/connection.php';
 $stmt = $pdo->query('SELECT * FROM usuario'); //execute essa ação //query->buscar
 $usuarios = $stmt->fetchAll(); //irá buscar todos os dados,tem um array 
 
-//$usuarios = {
-//     'id ' => 0,
-//     'username' =>
-// }
 
-// echo "<pre>";
-// echo var_dump($usuarios);  
+if( isset($_GET{'id'})){
+    $id = $_GET['id'];
+    echo "O id $id foi removido!";
+}
+
 ?>
 
 
 <?php  foreach ($usuarios as $indice => $user) {?>
     <p>
         <strong>Nome da Heroina:</strong><?php echo $user['username'];?>
+        <a href="http://localhost/PWII/PDO_CRUD/public/usuario/delete.php?id=<?php echo $user['id'];?>">Remover</a>
+        <a href="">Editar</a>
     </p>
     <hr>
 <?php } ?>
